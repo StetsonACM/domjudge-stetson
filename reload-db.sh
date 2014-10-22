928203d2a40e0abfc546dee8aaee8d71d5154a01
Joshua Eckroth <eckroth@cse.ohio-state.edu>
2014-10-22 08:44:12 -0400

    More minor tweaks and a script to reload the db.

---
+++ reload-db.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+MYSQLROOT=`grep -oE 'mysql_root_password: .*' ansible/vars/domserver-passwords.yml | sed -e 's/mysql_root_password: //'`
+SCPOPTS=`vagrant ssh-config domserver | grep -v '^Host ' | grep -v '^$' | awk -v ORS=' ' '{print "-o " $1 "=" $2}'`
+IP=`echo ${SCPOPTS} | grep -oE '((1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])\.){3}(1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])'`
+scp ${SCPOPTS} domjudge.sql vagrant@${IP}:/home/vagrant/domjudge.sql
+vagrant ssh domserver -- "mysql -u root -p${MYSQLROOT} domjudge < /home/vagrant/domjudge.sql"
+vagrant ssh domserver -- "rm -f /home/vagrant/domjudge.sql"
+
+## restart judgedaemons
+for j in `vagrant status | grep -oE '(judgehost[0-9]+)'`
+do
+  vagrant ssh $j -- "sudo service judgehost restart"
+done
+
